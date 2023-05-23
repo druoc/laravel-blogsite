504d0c3933db65e0059418ae1300f2ed166fbb6d
Dave <d.ruocco1987@googlemail.com>
2023-05-23 12:08:07 +0100

    Sets up homepage route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\testcontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [testcontroller::class, "homepage"]);
 
 Route::get('/about', function () {
     return '<h1>About page</h1>';
